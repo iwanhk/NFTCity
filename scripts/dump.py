bae3d79678b3475cc0f6333701f371b574085282
iwanhk <iwan@isotop.top>
2022-04-30 19:45:03 +0800

    temp

--- scripts/dump.py
+++ scripts/dump.py
@@ -4,13 +4,18 @@ import json
 import os,sys
 import random
 from scripts.gen_gif import gen_dir
+<<<<<<< Updated upstream
 import scripts.city_zone
+=======
+import scripts.city_zone as city_zone
+>>>>>>> Stashed changes
 
 D18= 10**18
 ZERO= '0x0000000000000000000000000000000000000000'
 active_network= network.show_active()
 LANG=["af", "sq", "am", "ar", "hy", "az", "eu", "be", "bn", "bs", "bg", "ca", "ceb", "ny", "zh-cn", "zh-tw", "co", "hr", "cs", "da", "nl", "en", "eo", "et", "tl", "fi", "fr", "fy", "gl", "ka", "de", "el", "gu", "ht", "ha", "haw", "iw", "he", "hi", "hmn", "hu", "is", "ig", "id", "ga", "it", "ja", "jw", "kn", "kk", "km", "ko", "ku", "ky", "lo", "la", "lv", "lt", "lb", "mk", "mg", "ms", "ml", "mt", "mi", "mr", "mn", "my", "ne", "no", "or", "ps", "fa", "pl", "pt", "pa", "ro", "ru", "sm", "gd", "sr", "st", "sn", "sd", "si", "sk", "sl", "so", "es", "su", "sw", "sv", "tg", "ta", "te", "th", "tr", "uk", "ur", "ug", "uz", "vi", "cy", "xh", "yi", "yo", "zu"];
 DATADIR= 'data/'
+ROOT='0x4a8d100c5b3c09841808d8fe60f6e7ce0812e6154420676e08030af0ad9b43fc'
 
 def process_city(meta_file:str):
     with open(meta_file,'r') as load_f:
@@ -27,7 +32,7 @@ def process_city(meta_file:str):
     print(f"{names_list=}")
     return (names_list, lan_list)
 
-def mint(city, cityDict, nft, reveal, user):
+def mint(city, cityDict, nft, user):
     file_name=os.path.join(DATADIR+ "city_meta", city+'.ot.json')
     names_list, lan_list= process_city(file_name)
     city_name, zone, now_time= city_zone.query(city, cityDict)
@@ -39,7 +44,9 @@ def mint(city, cityDict, nft, reveal, user):
     zoneDiff=int(now_time[-4:-2])*60+ int(now_time[-2:])
     if now_time[-5]=='-':
         zoneDiff= -zoneDiff
-    nft.mint(names_list, zoneDiff, lan_list, reveal, addr(user))
+    #nft.mint(names_list, zoneDiff, lan_list, reveal, addr(user))
+    nft.publicSaleMint(user, names_list, zoneDiff, lan_list, addr2(user, 0))
+    
 
 def dump_svg(nft, index, user):
     # Dump SVG
@@ -92,13 +99,21 @@ def main():
             Random.deploy(addr(admin))
             SVG.deploy(addr(admin))
             DateTime.deploy(addr(admin))
-            nft= CivCityNFT.deploy(addr(admin))
+            team= [admin, creator]
+            share= [50, 50]
+            CivCityNFT.deploy(team, share, ROOT, addr(admin))
 
+            nft= CivCityNFT[-1]
+            nft.setStep(3) # Final stage
+            nft.setPrice(0,0)
+
+            """
             for city in cities:
                 if os.path.exists(DATADIR+ 'svg/'+ city):
                     continue
-                mint(city, cityDict, nft, True, admin)
-            #mint(cities[0], nft, admin)
+                mint(city, cityDict, nft, admin)
+            """
+            mint(cities[0],cityDict, nft, admin)
 
             for i in range(nft.totalSupply()):
                 name= nft.getNames(i)[nft.getLangs(i)[21]]
